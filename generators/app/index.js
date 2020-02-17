fa11a2c429cf48a4857ba37f871b7ea85673c52b
Will Buck <william.parker.buck@gmail.com>
2020-02-17 14:51:15 -0600

    #9 wip adding files

---
+++ generators/app/index.js
@@ -0,0 +1,144 @@
+/* eslint-disable consistent-return */
+const chalk = require('chalk');
+const AppGenerator = require('generator-jhipster/generators/app');
+const jhipsterPackagejs = require('generator-jhipster/package.json');
+
+module.exports = class extends AppGenerator {
+    /**
+     * Override yeoman standard storage function for yo-rc.json
+     *  in order to save variables in generator-jhipster key.
+     * @return {String} The name of the root generator
+     */
+    rootGeneratorName() {
+        return jhipsterPackagejs.name;
+    }
+
+    constructor(args, opts) {
+        super(args, Object.assign({ fromBlueprint: true }, opts)); // fromBlueprint variable is important
+
+        const jhContext = (this.jhipsterContext = this.options.jhipsterContext);
+
+        if (!jhContext) {
+            this.error(`This is a JHipster blueprint and should be used only like ${chalk.yellow('jhipster --blueprints micronaut')}`);
+        }
+
+        this.configOptions = jhContext.configOptions || {};
+    }
+
+    get initializing() {
+        const initPhaseFromJHipster = this._initializing();
+        this.log(chalk.white('Initializing...'));
+
+        const jhipsterInitAppPhaseSteps = {
+            displayLogo() {
+                this.log('\n');
+                this.log(`${chalk.blue(' ███╗   ███╗')}${chalk.green(' ██╗   ██╗ ████████╗ ███████╗   ██████╗ ████████╗ ████████╗ ███████╗')}`);
+                this.log(`${chalk.blue(' ████╗ ████║')}${chalk.green(' ██║   ██║ ╚══██╔══╝ ██╔═══██╗ ██╔════╝ ╚══██╔══╝ ██╔═════╝ ██╔═══██╗')}`);
+                this.log(`${chalk.blue(' ██╔████╔██║')}${chalk.green(' ████████║    ██║    ███████╔╝ ╚█████╗     ██║    ██████╗   ███████╔╝')}`);
+                this.log(`${chalk.blue(' ██║╚██╔╝██║')}${chalk.green(' ██╔═══██║    ██║    ██╔════╝   ╚═══██╗    ██║    ██╔═══╝   ██╔══██║')}`);
+                this.log(`${chalk.blue(' ██║ ╚═╝ ██║')}${chalk.green(' ██║   ██║ ████████╗ ██║       ██████╔╝    ██║    ████████╗ ██║  ╚██╗')}`);
+                this.log(`${chalk.blue(' ╚═╝     ╚═╝')}${chalk.green(' ╚═╝   ╚═╝ ╚═══════╝ ╚═╝       ╚═════╝     ╚═╝    ╚═══════╝ ╚═╝   ╚═╝')}\n`);
+                this.log(chalk.white.bold('                            https://www.jhipster.tech\n'));
+                this.log(chalk.white('Welcome to MHipster  🤓  :: Running Micronaut version 1.3.0'));
+                this.log(chalk.white('This blueprint generates your backend as a Micronaut Java project.'));
+                this.log(chalk.green(' _______________________________________________________________________________________________________________\n'));
+                this.log(chalk.white(`  For any questions or improvements refer to the stream lead at ${chalk.yellow('https://github.com/willbuck')}`));
+                this.log(
+                    chalk.white(
+                        `  If you find MHipster useful, support and star the project at ${chalk.yellow(
+                            'https://github.com/jhipster/generator-jhipster-micronaut'
+                        )}`
+                    )
+                );
+                this.log(
+                    chalk.green(
+                        ' _______________________________________________________________________________________________________________\n'
+                    )
+                );
+            }
+        };
+
+        return Object.assign(initPhaseFromJHipster, jhipsterInitAppPhaseSteps);
+    }
+
+    get prompting() {
+        this.log(chalk.white('Prompting...'));
+        // If the prompts need to be overriden then use the code commented out above instead
+        return super._prompting();
+    }
+
+    get configuring() {
+        const configuringPhaseFromJHipster = super._configuring();
+        this.log(chalk.white('Configuring...'));
+
+        const jhipsterConfigureAppPhaseSteps = {
+            composeServer() {
+                if (this.skipServer) return;
+                const options = this.options;
+                const configOptions = this.configOptions;
+
+                this.composeWith(require.resolve('../server'), {
+                    ...options,
+                    configOptions,
+                    'client-hook': !this.skipClient,
+                    debug: this.isDebugEnabled
+                });
+            },
+
+            composeClient() {
+                if (this.skipClient) return;
+                const options = this.options;
+                const configOptions = this.configOptions;
+
+                this.composeWith(require.resolve('../client'), {
+                    ...options,
+                    configOptions,
+                    debug: this.isDebugEnabled
+                });
+            },
+
+            composeCommon() {
+                const options = this.options;
+                const configOptions = this.configOptions;
+
+                this.composeWith(require.resolve('../common'), {
+                    ...options,
+                    'client-hook': !this.skipClient,
+                    configOptions,
+                    debug: this.isDebugEnabled
+                });
+            }
+        };
+
+        return Object.assign(configuringPhaseFromJHipster, jhipsterConfigureAppPhaseSteps);
+    }
+
+    get default() {
+        const defaultPhaseFromJHipster = super._default();
+        const jhipsterConfigureAppPhaseSteps = {
+
+        };
+
+        this.log(chalk.white('Default...'));
+
+        return Object.assign(defaultPhaseFromJHipster, jhipsterConfigureAppPhaseSteps);
+    }
+
+    get writing() {
+        const writingPhaseFromJHipster = super._writing();
+
+        const jhipsterWritingAppPhaseSteps = {
+
+        };
+
+        this.log(chalk.white('Writing...'));
+
+        return Object.assign(writingPhaseFromJHipster, jhipsterWritingAppPhaseSteps);
+    }
+
+    get end() {
+        // Here we are not overriding this phase and hence its being handled by JHipster
+        this.log(chalk.white('Ending...'));
+        return super._end();
+    }
+};
---
+++ generators/client/files.js
@@ -0,0 +1,63 @@
+const jhipsterConstants = require('generator-jhipster/generators/generator-constants');
+
+const ANGULAR_DIR = jhipsterConstants.ANGULAR_DIR;
+const REACT_DIR = jhipsterConstants.REACT_DIR;
+
+const clientFiles = {
+    common: [
+        {
+            condition: generator => generator.clientFramework === 'angularX',
+            templates: [
+                { file: 'angular/tsconfig-aot.json', renameTo: () => 'tsconfig-aot.json' },
+                { file: 'angular/tsconfig.json', renameTo: () => 'tsconfig.json' },
+                { file: 'angular/package.json', renameTo: () => 'package.json' },
+                { file: 'angular/.eslintignore', renameTo: () => '.eslintignore' }
+            ]
+        },
+        {
+            condition: generator => generator.clientFramework === 'react',
+            templates: [
+                { file: 'react/tsconfig.json', renameTo: () => 'tsconfig.json' },
+                { file: 'react/package.json', renameTo: () => 'package.json' },
+                { file: 'react/.eslintrc.json', renameTo: () => '.eslintrc.json' },
+                { file: 'react/.eslintignore', renameTo: () => '.eslintignore' }
+            ]
+        }
+    ],
+    angularMain: [
+        {
+            condition: generator => generator.clientFramework === 'angularX',
+            templates: [
+                {
+                    file: 'angular/home/home.component.html',
+                    method: 'processHtml',
+                    renameTo: () => `${ANGULAR_DIR}home/home.component.html`
+                }
+            ]
+        }
+    ],
+    reactMain: [
+        {
+            condition: generator => generator.clientFramework === 'react',
+            templates: [
+                {
+                    file: 'react/home/home.tsx',
+                    method: 'processJsx',
+                    renameTo: () => `${REACT_DIR}modules/home/home.tsx`
+                }
+            ]
+        }
+    ]
+};
+
+function writeFiles() {
+    return {
+        overrideFiles() {
+            this.writeFilesToDisk(clientFiles, this, false);
+        }
+    };
+}
+
+module.exports = {
+    writeFiles
+};
