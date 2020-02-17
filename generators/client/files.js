aaff4d693db503663bf85c12a2b09a468c9ee712
Will Buck <william.parker.buck@gmail.com>
2020-02-17 16:41:29 -0600

    #9 completes generating an app that runs and allows login, TODO does not support entities, our config customizations

--- generators/client/files.js
+++ generators/client/files.js
@@ -4,26 +4,6 @@ const ANGULAR_DIR = jhipsterConstants.ANGULAR_DIR;
 const REACT_DIR = jhipsterConstants.REACT_DIR;
 
 const clientFiles = {
-    common: [
-        {
-            condition: generator => generator.clientFramework === 'angularX',
-            templates: [
-                { file: 'angular/tsconfig-aot.json', renameTo: () => 'tsconfig-aot.json' },
-                { file: 'angular/tsconfig.json', renameTo: () => 'tsconfig.json' },
-                { file: 'angular/package.json', renameTo: () => 'package.json' },
-                { file: 'angular/.eslintignore', renameTo: () => '.eslintignore' }
-            ]
-        },
-        {
-            condition: generator => generator.clientFramework === 'react',
-            templates: [
-                { file: 'react/tsconfig.json', renameTo: () => 'tsconfig.json' },
-                { file: 'react/package.json', renameTo: () => 'package.json' },
-                { file: 'react/.eslintrc.json', renameTo: () => '.eslintrc.json' },
-                { file: 'react/.eslintignore', renameTo: () => '.eslintignore' }
-            ]
-        }
-    ],
     angularMain: [
         {
             condition: generator => generator.clientFramework === 'angularX',
@@ -32,6 +12,11 @@ const clientFiles = {
                     file: 'angular/home/home.component.html',
                     method: 'processHtml',
                     renameTo: () => `${ANGULAR_DIR}home/home.component.html`
+                },
+                {
+                    file: 'angular/src/main/webapp/app/core/auth/auth-jwt.service.ts',
+                    method: 'processJs',
+                    renameTo: () => `${ANGULAR_DIR}core/auth/auth-jwt.service.ts`
                 }
             ]
         }
