573c011895a78f0e67ada58d1179e383f496c8f3
Will Buck <william.parker.buck@gmail.com>
2020-02-17 14:51:15 -0600

    #9 wip adding files

---
+++ generators/common/index.js
@@ -0,0 +1,43 @@
+/* eslint-disable consistent-return */
+const chalk = require('chalk');
+const CommonGenerator = require('generator-jhipster/generators/common');
+const writeFiles = require('./files').writeFiles;
+
+module.exports = class extends CommonGenerator {
+    constructor(args, opts) {
+        super(args, Object.assign({ fromBlueprint: true }, opts)); // fromBlueprint variable is important
+
+        const jhContext = (this.jhipsterContext = this.options.jhipsterContext);
+
+        if (!jhContext) {
+            this.error(`This is a JHipster blueprint and should be used only like ${chalk.yellow('jhipster --blueprints micronaut')}`);
+        }
+
+        this.configOptions = jhContext.configOptions || {};
+        // This sets up options for this sub generator and is being reused from JHipster
+        jhContext.setupServerOptions(this, jhContext);
+        jhContext.setupClientOptions(this, jhContext);
+    }
+
+    get initializing() {
+        const initPhaseFromJHipster = super._initializing();
+        const initMicronautCommonPhaseSteps = {
+
+        };
+        return Object.assign(initPhaseFromJHipster, initMicronautCommonPhaseSteps);
+
+        // Here we are not overriding this phase and hence its being handled by JHipster
+        // return super._initializing();
+    }
+
+    get default() {
+        // Here we are not overriding this phase and hence its being handled by JHipster
+        return super._default();
+    }
+
+    get writing() {
+        const phaseFromJHipster = super._writing();
+        const jhipsterMicronautCommonPhaseSteps = writeFiles();
+        return Object.assign(phaseFromJHipster, jhipsterMicronautCommonPhaseSteps);
+    }
+};
