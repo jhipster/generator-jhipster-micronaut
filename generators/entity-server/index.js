a18a0bf099efc5c75c2a19e40ca44f60446a1377
Will Buck <william.parker.buck@gmail.com>
2020-02-27 16:38:50 -0600

    #12 WIP entity generation throwing w/ entityResource

--- generators/entity-server/index.js
+++ generators/entity-server/index.js
@@ -1,61 +1,36 @@
+/**
+ * Copyright 2013-2020 the original author or authors from the JHipster project.
+ *
+ * This file is part of the JHipster project, see https://www.jhipster.tech/
+ * for more information.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /* eslint-disable consistent-return */
 const chalk = require('chalk');
 const EntityServerGenerator = require('generator-jhipster/generators/entity-server');
+const writeFiles = require('./files').writeFiles;
 
 module.exports = class extends EntityServerGenerator {
     constructor(args, opts) {
-        super(args, Object.assign({ fromBlueprint: true }, opts)); // fromBlueprint variable is important
-
-        const jhContext = (this.jhipsterContext = this.options.jhipsterContext);
-
-        if (!jhContext) {
-            this.error(`This is a JHipster blueprint and should be used only like ${chalk.yellow('jhipster --blueprint micronaut')}`);
-        }
-
-        this.configOptions = jhContext.configOptions || {};
-        if (jhContext.databaseType === 'cassandra') {
-            this.pkType = 'UUID';
+        super(args, { fromBlueprint: true, ...opts }); // fromBlueprint variable is important
+        if (!this.jhipsterContext) {
+            this.error(`This is a JHipster blueprint and should be used like ${chalk.yellow('mhipster')} 
+                        or ${chalk.yellow('jhipster --blueprint micronaut')}`);
         }
     }
 
     get writing() {
-        /**
-         * Any method beginning with _ can be reused from the superclass `EntityServerGenerator`
-         *
-         * There are multiple ways to customize a phase from JHipster.
-         *
-         * 1. Let JHipster handle a phase, blueprint doesnt override anything.
-         * ```
-         *      return super._writing();
-         * ```
-         *
-         * 2. Override the entire phase, this is when the blueprint takes control of a phase
-         * ```
-         *      return {
-         *          myCustomWritePhaseStep() {
-         *              // Do all your stuff here
-         *          },
-         *          myAnotherCustomWritePhaseStep(){
-         *              // Do all your stuff here
-         *          }
-         *      };
-         * ```
-         *
-         * 3. Partially override a phase, this is when the blueprint gets the phase from JHipster and customizes it.
-         * ```
-         *      const phaseFromJHipster = super._writing();
-         *      const myCustomPhaseSteps = {
-         *          writeClientFiles() {
-         *              // override the writeClientFiles method from the _writing phase of JHipster
-         *          },
-         *          myCustomInitPhaseStep() {
-         *              // Do all your stuff here
-         *          },
-         *      }
-         *      return Object.assign(phaseFromJHipster, myCustomPhaseSteps);
-         * ```
-         */
-        // Here we are not overriding this phase and hence its being handled by JHipster
-        return super._writing();
+        return writeFiles();
     }
 };
