c94cc874048c1ed5da08afd2a5c40ff42922fb7c
Aurélien Mino <aurelien.mino@gmail.com>
2020-10-14 22:33:23 +0200

    Fix gradle CI build with skipClient == true

--- test-integration/scripts/04-server-tests.sh
+++ test-integration/scripts/04-server-tests.sh
@@ -9,13 +9,18 @@ source $UTIL_FOLDER/jdl_util.sh
 
 cd ./sample-output/$1
 buildTool=`jdlVal "./$1.jdl" "buildTool"`
+skipClient=`jdlVal "./$1.jdl" "skipClient"`
 
 echo "Running $buildTool server tests for $1..."
 
 if [ "$buildTool" == "maven" ]; then
     ./mvnw -ntp clean verify
 else
-    ./gradlew test integrationTest -x webpack -x webpackBuildDev
+    if [ "$skipClient" == "true" ]; then
+        ./gradlew test integrationTest
+    else
+        ./gradlew test integrationTest -x webpack -x webpackBuildDev
+    fi
 fi
 
 cd ../../
