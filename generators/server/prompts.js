24debb8cb8f873f3717e107261c2e35fc17bf01f
Aurélien Mino <aurelien.mino@gmail.com>
2020-10-20 09:41:53 +0200

    Microservice support (#188)
    * Support for skipUserManagement and server-side skipClient
    
    * Only include org.mindrot:jbcrypt if user management is enabled
    
    * Enable microservice application type
    
    * Enable serviceDiscovery
    
    * Fix distributed configuration with Consul
    
    * Allow Micronaut microservice to use JWT token generated by a JHipster/Spring gateway
    
    * Swagger support
    
    * Fix OAuth2 & skipUserManagement
    
    * Fix consul
    
    * Enable OAuth2 for microservice
    
    * Add CI sample for microservice with JWT auth and Consul
    
    * Support for eureka service discovery
    
    * Add second sample for microservice (oauth2+eureka)
    
    * Fix gradle CI build with skipClient == true
    
    * Disable distributed configuration for tests
    
    * Move application-test.yml to test resources directory

--- generators/server/prompts.js
+++ generators/server/prompts.js
@@ -54,8 +54,8 @@ function askForServerSideOpts(meta) {
         },
     ];
 
-    if (applicationType !== 'monolith') {
-        logger.error('Application should be only monolith for this blueprint');
+    if (applicationType !== 'monolith' && applicationType !== 'microservice') {
+        logger.error('Application should be only monolith or microservice for this blueprint');
         return;
     }
 
@@ -66,7 +66,7 @@ function askForServerSideOpts(meta) {
     }
     const prompts = [
         {
-            when: response => applicationType === 'gateway' || applicationType === 'microservice' || applicationType === 'uaa',
+            when: () => applicationType === 'gateway' || applicationType === 'microservice' || applicationType === 'uaa',
             type: 'input',
             name: 'serverPort',
             validate: input => (/^([0-9]*)$/.test(input) ? true : 'This is not a valid port number.'),
@@ -85,28 +85,27 @@ function askForServerSideOpts(meta) {
             default: 'com.mycompany.myapp',
             store: true,
         },
-        // TODO enable it when we allow other apps than monoliths
-        // {
-        //     when: response => applicationType === 'gateway' || applicationType === 'microservice' || applicationType === 'uaa',
-        //     type: 'list',
-        //     name: 'serviceDiscoveryType',
-        //     message: 'Which service discovery server do you want to use?',
-        //     choices: [
-        //         {
-        //             value: 'eureka',
-        //             name: 'JHipster Registry (uses Eureka, provides Spring Cloud Config support and monitoring dashboards)'
-        //         },
-        //         {
-        //             value: 'consul',
-        //             name: 'Consul'
-        //         },
-        //         {
-        //             value: false,
-        //             name: 'No service discovery'
-        //         }
-        //     ],
-        //     default: 'eureka'
-        // },
+        {
+            when: () => applicationType === 'gateway' || applicationType === 'microservice' || applicationType === 'uaa',
+            type: 'list',
+            name: 'serviceDiscoveryType',
+            message: 'Which service discovery server do you want to use?',
+            choices: [
+                {
+                    value: 'eureka',
+                    name: 'JHipster Registry (uses Eureka, provides Spring Cloud Config support and monitoring dashboards)',
+                },
+                {
+                    value: 'consul',
+                    name: 'Consul',
+                },
+                {
+                    value: false,
+                    name: 'No service discovery',
+                },
+            ],
+            default: 'eureka',
+        },
         // {
         //     when: applicationType === 'monolith',
         //     type: 'list',
@@ -143,12 +142,13 @@ function askForServerSideOpts(meta) {
                         value: 'oauth2',
                         name: 'OAuth 2.0 / OIDC Authentication (stateful, works with Keycloak and Okta)',
                     });
-                    if (['gateway', 'microservice'].includes(applicationType)) {
-                        opts.push({
-                            value: 'uaa',
-                            name: 'Authentication with JHipster UAA server (the server must be generated separately)',
-                        });
-                    }
+                    // FIXME: UAA is not yet supported
+                    // if (['gateway', 'microservice'].includes(applicationType)) {
+                    //     opts.push({
+                    //         value: 'uaa',
+                    //         name: 'Authentication with JHipster UAA server (the server must be generated separately)',
+                    //     });
+                    // }
                 }
                 return opts;
             },
@@ -179,7 +179,7 @@ function askForServerSideOpts(meta) {
                 if (!reactive) {
                     opts.push({
                         value: 'sql',
-                        name: 'SQL (H2, MySQL, PostgreSQL)',
+                        name: 'SQL (H2, MySQL, MariaDB, PostgreSQL)',
                     });
                 }
                 // TODO enable when we support these things
@@ -256,7 +256,7 @@ function askForServerSideOpts(meta) {
                     name: 'No - Warning, when using an SQL database, this will disable the Hibernate 2nd level cache!',
                 },
             ],
-            default: applicationType === 'microservice' || applicationType === 'uaa' ? 1 : 0,
+            default: applicationType === 'microservice' || applicationType === 'uaa' ? 2 : 0,
         },
         {
             when: response =>
