1b029ed11cbc46e34d9fa0751ed803074feb2db6
Marcelo Shima <marceloshima@gmail.com>
2022-04-13 10:25:23 -0300

    Generator cannot be empty

--- generators/database-changelog/generator.mjs
+++ generators/database-changelog/generator.mjs
@@ -1,6 +1,6 @@
 import chalk from 'chalk';
 import DatabaseChangelogGenerator from 'generator-jhipster/esm/generators/database-changelog';
-import { PRIORITY_PREFIX } from 'generator-jhipster/esm/priorities';
+import { PRIORITY_PREFIX, INITIALIZING_PRIORITY } from 'generator-jhipster/esm/priorities';
 
 export default class extends DatabaseChangelogGenerator {
   constructor(args, opts, features) {
@@ -11,5 +11,9 @@ export default class extends DatabaseChangelogGenerator {
     if (!this.options.jhipsterContext) {
       throw new Error(`This is a JHipster blueprint and should be used only like ${chalk.yellow('jhipster --blueprints micronaut')}`);
     }
+  }
+
+  get [INITIALIZING_PRIORITY]() {
+    return {};
   }
 }
