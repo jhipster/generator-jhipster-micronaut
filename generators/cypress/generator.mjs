3813357c375495949fd759cbc1e4f6ef3a29f5b9
Marcelo Shima <marceloshima@gmail.com>
2023-09-21 16:40:40 -0300

    adjust cypress

--- generators/cypress/generator.mjs
+++ generators/cypress/generator.mjs
@@ -8,11 +8,11 @@ export default class extends BaseApplicationGenerator {
   get [BaseApplicationGenerator.POST_WRITING]() {
     return this.asPostWritingTaskGroup({
       customizeCypress({ application }) {
+        this.editFile(`${application.cypressDir}e2e/administration/administration.cy.ts`, content =>
+          content.replace("describe('/docs'", "describe.skip('/docs'"),
+        );
         if (application.authenticationTypeJwt) {
           this.editFile(`${application.cypressDir}support/commands.ts`, content => content.replaceAll('id_token', 'access_token'));
-          this.editFile(`${application.cypressDir}e2e/administration/administration.cy.ts`, content =>
-            content.replace("describe('/docs'", "describe.skip('/docs'"),
-          );
         }
       },
     });
