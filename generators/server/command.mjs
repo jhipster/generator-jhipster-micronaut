4868094b8bcb8477f21910849105ee025b5a7b19
Marcelo Shima <marceloshima@gmail.com>
2023-09-22 12:19:27 -0300

    command

--- generators/server/command.mjs
+++ generators/server/command.mjs
@@ -1,8 +1,21 @@
+import { command as serverCommand } from 'generator-jhipster/generators/server';
+
+const { applicationType, ...remainingConfigs } = serverCommand.configs;
+
 /**
  * @type {import('generator-jhipster').JHipsterCommandDefinition}
  */
 const command = {
-  options: {},
+  ...serverCommand,
+  configs: {
+    // Gateway is not supported
+    applicationType: {
+      ...applicationType,
+      choices: applicationType.choices.filter(({ value }) => value !== 'gateway'),
+    },
+    ...remainingConfigs,
+  },
+  override: true,
 };
 
 export default command;
