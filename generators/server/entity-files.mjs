b88baed2dba991a83103fd5b62bec0c809125585
Marcelo Shima <marceloshima@gmail.com>
2023-09-22 12:19:20 -0300

    prettier

--- generators/server/entity-files.mjs
+++ generators/server/entity-files.mjs
@@ -16,10 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {
-  javaMainPackageTemplatesBlock,
-  javaTestPackageTemplatesBlock,
-} from 'generator-jhipster/generators/java/support';
+import { javaMainPackageTemplatesBlock, javaTestPackageTemplatesBlock } from 'generator-jhipster/generators/java/support';
 
 const TEST_DIR = 'src/test/';
 
