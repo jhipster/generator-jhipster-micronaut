914c62b733f7cfb1e132c95d0368079b0625a10d
Marcelo Shima <marceloshima@gmail.com>
2023-10-15 12:21:34 -0300

    add entity test

--- generators/server/generator.spec.mjs
+++ generators/server/generator.spec.mjs
@@ -23,4 +23,42 @@ describe('SubGenerator server of micronaut JHipster blueprint', () => {
       expect(result.getStateSnapshot()).toMatchSnapshot();
     });
   });
+
+  describe('with some entities', () => {
+    beforeAll(async function () {
+      await helpers
+        .run(BLUEPRINT_NAMESPACE)
+        .withJHipsterConfig(
+          {
+            creationTimestamp: 1596513172471,
+          },
+          [
+            {
+              name: 'Entity',
+              fields: [
+                {
+                  fieldName: 'name',
+                  fieldType: 'String',
+                },
+              ],
+            },
+          ],
+        )
+        .withOptions({
+          reproducible: true,
+          ignoreNeedlesError: true,
+          blueprint: 'micronaut',
+        })
+        .withJHipsterLookup()
+        .withParentBlueprintLookup();
+    });
+
+    it('should succeed', () => {
+      expect(result.getStateSnapshot()).toMatchSnapshot();
+    });
+
+    it('should not write jakarta validation', () => {
+      expect(result.fs.read('src/main/java/com/mycompany/myapp/domain/Entity.java.jhi')).toMatch(/^((?!jakarta).)*$/s);
+    });
+  });
 });
