f820c0f4c8bccd596024cdb5c3585d1491a5757c
Marcelo Shima <marceloshima@gmail.com>
2024-04-17 14:22:14 -0300

    prompt adjusts

--- generators/server/command.js
+++ generators/server/command.js
@@ -1,9 +1,21 @@
+import { command as serverCommand } from 'generator-jhipster/generators/server';
+
+const { applicationType, ...remainingConfigs } = serverCommand.configs;
+
 /**
  * @type {import('generator-jhipster').JHipsterCommandDefinition}
  */
 const command = {
-  options: {},
-  configs: {},
+  ...serverCommand,
+  configs: {
+    // Gateway is not supported
+    applicationType: {
+      ...applicationType,
+      choices: applicationType.choices.filter(({ value }) => value !== 'gateway'),
+    },
+    ...remainingConfigs,
+  },
+  override: true,
   compose: ['jhipster-micronaut:micronaut'],
 };
 
