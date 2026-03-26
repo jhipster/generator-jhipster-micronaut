2d3c1203c66ced77f5c3900c4216bee14d7a11b9
Marcelo Shima <marceloshima@gmail.com>
2026-03-26 15:53:38 -0300

    rename bootstrap-application-base to base-application:bootstrap

---
+++ generators/base-application/generators/bootstrap/generator.spec.js
@@ -0,0 +1,26 @@
+import { beforeAll, describe, expect, it } from 'vitest';
+
+import { defaultHelpers as helpers, result } from 'generator-jhipster/testing';
+
+const SUB_GENERATOR = 'base-application:bootstrap';
+const BLUEPRINT_NAMESPACE = `jhipster:${SUB_GENERATOR}`;
+
+describe('SubGenerator base-application:bootstrap of micronaut JHipster blueprint', () => {
+  describe('run', () => {
+    beforeAll(async function () {
+      await helpers
+        .run(BLUEPRINT_NAMESPACE)
+        .withJHipsterConfig()
+        .withOptions({
+          ignoreNeedlesError: true,
+        })
+        .withJHipsterGenerators()
+        .withConfiguredBlueprint()
+        .withBlueprintConfig();
+    });
+
+    it('should succeed', () => {
+      expect(result.getStateSnapshot()).toMatchSnapshot();
+    });
+  });
+});
