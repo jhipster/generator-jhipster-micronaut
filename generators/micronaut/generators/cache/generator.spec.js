8887f527ded98dfa3339206a40fce670a40fb5b4
Marcelo Shima <marceloshima@gmail.com>
2026-03-29 09:08:13 -0300

    move micronaut-cache into micronaut (#574)

--- generators/micronaut/generators/cache/generator.spec.js
+++ generators/micronaut/generators/cache/generator.spec.js
@@ -2,10 +2,10 @@ import { beforeAll, describe, expect, it } from 'vitest';
 
 import { defaultHelpers as helpers, result } from 'generator-jhipster/testing';
 
-const SUB_GENERATOR = 'micronaut-cache';
+const SUB_GENERATOR = 'micronaut:cache';
 const SUB_GENERATOR_NAMESPACE = `jhipster-micronaut:${SUB_GENERATOR}`;
 
-describe('SubGenerator micronaut-cache of micronaut JHipster blueprint', () => {
+describe('SubGenerator micronaut:cache of micronaut JHipster blueprint', () => {
   describe('run', () => {
     beforeAll(async function () {
       await helpers
