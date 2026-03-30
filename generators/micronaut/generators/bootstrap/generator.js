0a9533bfd2c15a1d2976c59bfcf311ec1374f699
Marcelo Shima <marceloshima@gmail.com>
2026-03-30 16:43:01 -0300

    adjusts

--- generators/micronaut/generators/bootstrap/generator.js
+++ generators/micronaut/generators/bootstrap/generator.js
@@ -9,19 +9,12 @@ export default class extends BaseApplicationGenerator {
     await this.dependsOnBootstrap('server');
   }
 
-  get [BaseApplicationGenerator.CONFIGURING]() {
-    return this.asPreparingTaskGroup({
-      async preparingMicronaut() {
-        this.jhipsterConfig.backendType = 'micronaut';
-      },
-    });
-  }
-
   get [BaseApplicationGenerator.LOADING]() {
     return this.asPreparingTaskGroup({
       async preparingMicronaut({ applicationDefaults }) {
         applicationDefaults({
           backendTypeJavaAny: true,
+          backendType: 'micronaut',
           hipster: 'jhipster_family_member_4',
           hipsterBugTrackerLink: 'https://github.com/jhipster/generator-jhipster-micronaut/issues?state=open',
           hipsterBugTrackerProductName: 'MHipster',
@@ -59,10 +52,6 @@ export default class extends BaseApplicationGenerator {
   get [BaseApplicationGenerator.PREPARING]() {
     return this.asPreparingTaskGroup({
       configure({ application }) {
-        // bootstrap-application-base generator does not respect current configured hipster value.
-        Object.assign(application, {
-          hipster: 'jhipster_family_member_4',
-        });
         if (application.authenticationType === 'oauth2') {
           application.syncUserWithIdp = true;
           application.generateBuiltInUserEntity = true;
